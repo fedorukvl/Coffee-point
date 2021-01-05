a4f254c4649456311c4b518ec99cdbd655f3a451
Vasiliy Fedoruk <fedorukvl@demliz.com>
2021-01-05 21:41:01 +0300

    Создана начальная структура проекта

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: '@import "@/media/sass/variables.sass";'
+            }
+        }
+    }
+}
