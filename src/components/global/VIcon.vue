12055ba3d3ea8ebf39087e232b32a55d6ab61a91
Vasiliy Fedoruk <fedorukvl@demliz.com>
2021-01-13 13:52:06 +0300

    Создан компонент иконки + добавлены svg-иконки

---
+++ src/components/global/VIcon.vue
@@ -0,0 +1,22 @@
+<template>
+    <svg 
+        v-bind:src="`@/media/images/svg-icons/${name}.svg`"
+        v-bind:title="`${title}`"
+    ></svg>
+</template>
+
+<script>
+export default {
+    name: 'VIcon',
+    props:{
+        name:{
+            type: String,
+            required: true
+        },
+        title:{
+            type: String,
+            default: none
+        }
+    }
+}
+</script>
